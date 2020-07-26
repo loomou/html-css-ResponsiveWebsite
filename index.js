23a32dcb6e00ec16fd1a13b0431714d4344f1e40
loomou <273226981@qq.com>
2020-07-26 23:04:19 +0800

    成功案例部分的实现

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ glide.on(["mount.after", "run.after"], () => {
     opacity: [0, 1],
     duration: 400,
     easing: "linear",
-    delay: anime.stagger(400, { start: 300 }),
+    delay: anime.stagger(400, {start: 300}),
     translateY: [anime.stagger([40, 10]), 0]
   });
 });
@@ -20,3 +20,19 @@ glide.on("run.before", () => {
 });
 
 glide.mount();
+
+const isotope = new Isotope(".cases", {
+  layoutMode: "fitRows",
+  itemSelector: ".case-item"
+});
+
+const filterBtns = document.querySelector(".filter-btns");
+filterBtns.addEventListener("click", e => {
+  let { target } = e;
+  const filterOption = target.getAttribute("data-filter");
+  if (filterOption) {
+    document.querySelectorAll(".filter-btn.active").forEach(btn => btn.classList.remove("active"));
+    target.classList.add("active");
+    isotope.arrange({filter: filterOption});
+  }
+})
